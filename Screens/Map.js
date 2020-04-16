061eb1c9b07c70a30b6c21c2b4d3c27a87fbaff2
ABDUL BASITH <basith.ambalavan@gmail.com>
2020-04-16 23:00:38 +0530

    g

--- Screens/Map.js
+++ Screens/Map.js
@@ -3,33 +3,128 @@
 /* github    : abdulbasitha */
 /* More Info : https://techzia.in */
 import React, { Component } from "react";
+import Geolocation from '@react-native-community/geolocation';
 import Block from '../components/Block';
 import Text from '../components/Text';
 import Input from '../components/Input';
 import Button from '../components/Button';
 import firebase, { auth } from "firebase";
 import config from '../config/firebase';
-import Map from '../components/ViewLocation';
+import FetchLocation from '../components/FetchLocation';
+import Maps from '../components/UsersMaps';
+import ViewLocation from '../components/ViewLocation';
+import Head from '../components/header';
 import { 
     View,
-    StyleSheet
+    StyleSheet,
+    AppState,
+    SafeAreaView,StatusBar
 } from "react-native";
 
 class MapLocation extends Component {
+    state = {
+        appState: AppState.currentState,
+        userLacation: null,
+        viewLocation:null,
+        userPlaces : []
+      }
+
+      getUserLocation= ()=>{
+
+        Geolocation.watchPosition(position =>{
+          console.log(position);
+        this.setState({
+          userLacation:{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }
+        });
+        fetch('https://react-native-cli.firebaseio.com/places.json',{
+          method:'POST',
+          body:JSON.stringify({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          })
+        })
+        .then(res => console.log(res))
+        .catch(err=> console.log(err));
+    
+       },
+       err=>{
+         console.log(err);
+       });
+      }
+      Call=()=>{
+        this.timer = setInterval(()=> this.Tracking(), 1000)
+       }
+      Tracking = ()=>{
+    
+    
+        fetch('https://react-native-cli.firebaseio.com/places.json')
+        .then(res => res.json())
+        .then(parsedResult => {
+          const placesArray= [];
+          for (const key in parsedResult){
+            placesArray.push({
+              latitude:parsedResult[key].latitude,
+              longitude:parsedResult[key].longitude,
+              id:key
+            })
+           console.log(placesArray);
+          }
+          this.setState({
+            viewLocation:{
+    
+              latitude:  placesArray.slice(-1)[0].latitude,
+              longitude:  placesArray.slice(-1)[0].longitude,
+              latitudeDelta: 0.095,
+              longitudeDelta: 0.055
+            }
+          });
+          this.setState({
+            userPlaces:placesArray
+          })
+        })
+        .catch(err=> console.log(err));
+    
+        console.log(this.state.viewLocation);
+      }
+
+      
     render() {
+        const toggleDrawer = () => {
+            navigation.openDrawer();
+          }
         return (
-            <View style={styles.container}>
-                <Map />
-            </View>
+        <SafeAreaView style={styles.container}>
+              <StatusBar backgroundColor="white" barStyle="dark-content" />
+                <Head Option={toggleDrawer}/>
+        <Block style={styles.map}>
+          <ViewLocation Location={this.state.viewLocation}/>
+
+        </Block>
+
+        <Button color="green" title="Track" onPress={this.Call}/>
+      </SafeAreaView>
         );
     }
 }
 export default MapLocation;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+    container:{
+  flex:1
+    },
+    map:{
+  flex:1,
+  
+    },
+    mapfetch:{
+      flex:1,
+      paddingTop:10
+  
     }
-});
+  });
+  
